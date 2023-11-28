2057cf04441d31539161659c47a4068ec89e3e1b
ASUS <srikuna127@gmail.com>
2023-11-29 03:33:47 +0530

    Implementing Circuit Breaker in OrderService

--- OrderService/src/main/java/com/shopingcard/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/shopingcard/OrderService/external/client/PaymentService.java
@@ -2,11 +2,13 @@ package com.shopingcard.OrderService.external.client;
 
 import com.shopingcard.OrderService.exception.CustomException;
 import com.shopingcard.OrderService.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
--- OrderService/src/main/java/com/shopingcard/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/shopingcard/OrderService/external/client/ProductService.java
@@ -1,12 +1,13 @@
 package com.shopingcard.OrderService.external.client;
 
 import com.shopingcard.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/api/products")
 public interface ProductService {
 
